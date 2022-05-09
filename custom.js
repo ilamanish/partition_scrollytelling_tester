f798922c1287cfa14cdcaa08ed579239251142c8
ilamanish <103348042+ilamanish@users.noreply.github.com>
2022-05-09 19:52:11 +0530

    Adding the map

--- custom.js
+++ custom.js
@@ -1,1 +1,13 @@
+// JavaScript for enabling the map on load. Change the access token and the web page.
 
+mapboxgl.accessToken = 'pk.eyJ1IjoiaWxhbWFuaXNoIiwiYSI6ImNsMXQ4eDliMzB6N3kzb3FxbzY3ZmN5bDYifQ.swLACYH2fKaTe93_6hQ5sg';
+
+//Set the initial map view
+const map = new mapboxgl.Map({
+  container: 'map', // container ID
+  style: 'mapbox://styles/ilamanish/cl2ysfeie000u15ntw58pgqnb', // style URL
+  center: [77.0688997, 20.5272803], // starting position [lng, lat]
+  zoom: 4, // starting zoom
+  bearing: 0, //controls the left-right rotation of the map in degrees
+  pitch: 0 //controls the up-down rotation of the map.
+});
